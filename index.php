595efd21b12270f367d2e15053feb012432b6112
Fardli Yansah <fadlyyansah86@gmail.com>
2022-07-01 22:26:45 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At porro illum, eum libero reiciendis quia, consectetur repellendus maiores, laudantium dignissimos minima sequi similique ipsa fugiat rerum ex in ipsum deleniti?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
